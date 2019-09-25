b431158c4682bfd3a4ed72f1aa66bb46ebfdf730
alenovan <novannanega@gmail.com>
2019-09-25 11:31:00 +0700

    tugas  startter v2

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -13,31 +13,35 @@ import android.view.View;
 import java.io.PushbackInputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    Integer active;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmetChange(new PushupFragment());
+        active = 1;
+        fragmetChange(new PushupFragment(),4);
     }
 
     public void pushupButton(View view) {
-        fragmetChange(new PushupFragment());
+        fragmetChange(new PushupFragment(),1);
     }
 
-    public  void fragmetChange(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.replace(R.id.frameChange,fragment,"RED_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+    public  void fragmetChange(Fragment fragment,int status){
+        if(active != status) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            fragmentTransaction.replace(R.id.frameChange, fragment, status+"");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            active = status;
+        }
     }
 
     public void dipsButton(View view) {
-        fragmetChange(new DipsFragment());
+        fragmetChange(new DipsFragment(),2);
     }
 
     public void handsButton(View view) {
-        fragmetChange(new HandstandFragment());
+        fragmetChange(new HandstandFragment(),3);
     }
 }
