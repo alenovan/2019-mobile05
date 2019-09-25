7f0561abd73c2a8d60093fcbcb9b312b50d4105d
alenovan <novannanega@gmail.com>
2019-09-25 10:09:58 +0700

    tugas  startter v1

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -1,8 +1,16 @@
 package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
+import polinema.ac.id.starterchapter05.fragments.HandstandFragment;
+import polinema.ac.id.starterchapter05.fragments.PushupFragment;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.PushbackInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmetChange(new PushupFragment());
+    }
+
+    public void pushupButton(View view) {
+        fragmetChange(new PushupFragment());
+    }
+
+    public  void fragmetChange(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.frameChange,fragment,"RED_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void dipsButton(View view) {
+        fragmetChange(new DipsFragment());
+    }
+
+    public void handsButton(View view) {
+        fragmetChange(new HandstandFragment());
     }
 }
